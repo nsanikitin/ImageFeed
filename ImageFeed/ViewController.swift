e979477365aa2c6a59e2e68e5a263f7b3386cfed
nsanikitin <nsa010995@gmail.com>
2024-01-06 17:56:21 +0300

    Inital commit

--- ImageFeed/ViewController.swift
+++ ImageFeed/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  ImageFeed
-//
-//  Created by Сергей Никитин on 04.01.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,7 +6,4 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
