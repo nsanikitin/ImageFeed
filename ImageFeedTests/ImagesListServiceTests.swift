379787485d65ee2e8cb5fa47c8dda17ad6e69a6a
nsanikitin <nsa010995@gmail.com>
2024-03-19 23:48:33 +0300

    Написан тест для отладки сетевой загрузки фотографий

---
+++ ImageFeedTests/ImagesListServiceTests.swift
@@ -0,0 +1,22 @@
+@testable import ImageFeed
+import XCTest
+
+final class ImagesListServiceTests: XCTestCase {
+    
+    func testExample() {
+        let service = ImagesListService()
+        
+        let expectation = self.expectation(description: "Wait for Notification")
+        NotificationCenter.default.addObserver(
+            forName: ImagesListService.didChangeNotification,
+            object: nil,
+            queue: .main) { _ in
+                expectation.fulfill()
+            }
+        
+        service.fetchPhotosNextPage()
+        wait(for: [expectation], timeout: 10)
+        
+        XCTAssertEqual(service.photos.count, 10)
+    }
+}
