2ce4421b21be81d1ae37b08c08d2ca7a5fc22fba
nsanikitin <nsa010995@gmail.com>
2024-03-28 23:30:10 +0300

    Revert "Написаны UI-тесты"
    This reverts commit e11da94ccfd11f24e4b3eeef47911e9d7696a34a.

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -20,14 +20,14 @@ final class ImageFeedUITests: XCTestCase {
         XCTAssertTrue(loginTextField.waitForExistence(timeout: 5))
         
         loginTextField.tap()
-        loginTextField.typeText("nsa010995@gmail.com")
+        loginTextField.typeText("login")
         webView.swipeUp()
         
         let passwordTextField = webView.descendants(matching: .secureTextField).element
         XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
         
         passwordTextField.tap()
-        passwordTextField.typeText("unsplashNsa12174741010995!!")
+        passwordTextField.typeText("password!")
         webView.swipeUp()
         
         webView.buttons["Login"].tap()
